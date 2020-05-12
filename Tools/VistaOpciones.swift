423ba1013fb01cb783ded61481171ca3b6c13510
AOx0 <50227494+AOx0@users.noreply.github.com>
2020-05-12 00:43:35 -0500

    Upload 1

---
+++ Tools/VistaOpciones.swift
@@ -0,0 +1,25 @@
+//
+//  TextEditor.swift
+//  Tools
+//
+//  Created by Alejandro D on 15/04/20.
+//  Copyright © 2020 Alejandro D. All rights reserved.
+//
+
+import SwiftUI
+
+struct VistaOpción1: View {
+    @EnvironmentObject var config : Data
+
+    var body: some View {
+        Text("Vista opción 1")
+    }
+}
+
+struct VistaOpción2: View {
+    @EnvironmentObject var config : Data
+
+    var body: some View {
+        Text("Vista opción 2")
+    }
+}
